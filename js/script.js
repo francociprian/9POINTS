58857a2807062f723457fab88e21e0c338e1c03c
Franco Ciprian <franco_ciprian@hotmail.com>
2021-12-28 18:27:54 -0300

    separando las media Queries, fixiando pagina proyectos, aplicando bem a las paginas

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-const navToggle = document.querySelector(".nav-Toggle");
-const navMenu = document.querySelector(".menuNav");
+const navToggle = document.querySelector(".menu__toggle");
+const navMenu = document.querySelector(".menu__lista");
 
 
     navToggle.addEventListener("click", () => {
-        navMenu.classList.toggle("nav-menu_visible");
+        navMenu.classList.toggle("menu__lista--visible");
 
-    if (navMenu.classList.contains("nav-menu_visible")) {
+    if (navMenu.classList.contains("menu__lista--visible")) {
         navToggle.setAttribute("aria-label", "Cerrar menú");}
     else {
          navToggle.setAttribute("aria-label", "Abrir menú");}
