b5856f1183e089a80689c66a94af4c849b7782b8
Franco Ciprian <franco_ciprian@hotmail.com>
2022-01-29 19:23:48 -0300

    modificando metodologia bem

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-const navToggle = document.querySelector(".menu__toggle");
+const navBurger = document.querySelector(".menu__toggle");
 const navMenu = document.querySelector(".menu__lista");
 
 
-navToggle.addEventListener("click", () => {
+navBurger.addEventListener("click", () => {
     navMenu.classList.toggle("menu__lista--visible");
 
     if (navMenu.classList.contains("menu__lista--visible")) {
-        navToggle.setAttribute("aria-label", "Cerrar menú");
+        navBurger.setAttribute("aria-label", "Cerrar menú");
     } else {
-        navToggle.setAttribute("aria-label", "Abrir menú");
+        navBurger.setAttribute("aria-label", "Abrir menú");
     }
 });
 
 
-// 
+// Animacion tipo maquina de escribir del inicio
 
 const CONTENT = "9points desarrolla soluciones de software a medida desde start-up hasta empresas establecidas a nivel mundial. Combinamos un diseño innovador con asesoramiento a nuestros clientes en todas las decisiones digitales y comerciales.";
 
@@ -21,7 +21,7 @@ let type = '<span>' + CONTENT.split('').join('</span><span>') + '</span>';
 
 
 $(type).hide().appendTo('#index_text').each(function (i) {
-    $(this).delay(33 * i).css({
+    $(this).delay(30 * i).css({
         display: 'inline',
         opacity: 0
     }).animate({
